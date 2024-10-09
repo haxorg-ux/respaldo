96d72ae74c8d7c59eb5e511bfb162f8f7a8e75e4
m4teo <jose@gmail.com>
2024-10-08 22:14:31 -0300

    add icon statusline

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -1,7 +1,8 @@
 require'nvim-web-devicons'.setup {}
 
 -- groups
-vim.cmd("highlight StatusLineMode guifg=#FFFFFF guibg=#808080")
+vim.cmd("highlight StatusLineMode guifg=#1e222a guibg=#abb2bf")
+vim.cmd("highlight StatusLineSep guifg=#242426 guibg=#abb2bf")
 vim.cmd("highlight StatusLineFiletype guifg=#414373 guibg=#242426")
 vim.cmd("highlight StatusLinePath guifg=#FFFFFF guibg=#242426")
 vim.cmd("highlight StatusLinePosition guifg=#FFFFFF guibg=#242426")
@@ -15,6 +16,12 @@ local function get_file_icon()
   return icon
 end
 
+local mode_icons = {
+  n = " ",
+  i = " ",
+  c = " ",
+}
+
 local modes = {
   n = 'Normal', -- Normal
   i = 'Insert', -- Insert 
@@ -23,13 +30,15 @@ local modes = {
 
 _G.get_mode = function()
   local mode = vim.fn.mode()
+  local mode_icon = mode_icons[mode] or ""
   local mode_text = modes[mode] or mode
-  local padding = "  "
-  return padding .. mode_text
+  local padding = " "
+  return padding .. " " .. mode_icon .. mode_text
 end
 
 
 vim.o.statusline = "%#StatusLineMode#%{v:lua.get_mode()} "
+vim.o.statusline = vim.o.statusline .. "%#StatusLineSep#"
 vim.o.statusline = vim.o.statusline .. "%#StatusLineFiletype# " .. get_file_icon() .. " "
 vim.o.statusline = vim.o.statusline .. "%#StatusLinePath#%t   "
 vim.o.statusline = vim.o.statusline .. "%=  "
